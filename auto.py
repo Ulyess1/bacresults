28db5eb2b1096fd581e6d1372ade8ecc33b6089f
Ulyess1 <hhj3861@gmail.com>
2025-09-07 19:05:06 +0100

    added section for bac svt and i added user can add note for 1er semestre and also for 2eme

--- auto.py
+++ auto.py
@@ -1,24 +1,66 @@
-Math = float(input("Math :").replace(",","-"))
-philo = float(input("Philosophie :").replace(",","-"))
-pc = float(input("Physique :").replace(",","-"))
-svt = float(input("science de la vie et de la terre :").replace(",","-"))
-anglais = float(input("Anglais").replace(",","-"))
-regional = float(input("entrez vote note du regional :").replace(",","-"))
-controle = float(input("entrez vote note du controle continue :").replace(",","-"))
-m = Math * 7
-pc = pc * 7
-s = svt * 5
-a = anglais * 2
-ph = philo * 2
-total = m + pc + s + a + ph
-national = total / 23 
-regcont = regional + controle 
-divregcont = regcont / 2
-moyennegeneral = national + divregcont
-lanotetotale = moyennegeneral / 2
-if lanotetotale >= 10:
- print("nje7ti",lanotetotale)
-elif lanotetotale < 10 and lanotetotale > 7 :
- print("rattrapage",lanotetotale)
+print("Select your Choice")
+print("1-Bac Science Pc")
+print("2-Bac Science Svt")
+choice = input("Enter 1 Or 2 :")
+
+if choice == "1" :
+ Math = float(input("Math :").replace(",","-"))
+ philo = float(input("Philosophie :").replace(",","-"))
+ pc = float(input("Physique :").replace(",","-"))
+ svt = float(input("science de la vie et de la terre :").replace(",","-"))
+ anglais = float(input("Anglais :").replace(",","-"))
+ regional = float(input("entrez vote note du regional :").replace(",","-"))
+ controle1 = float(input("entrez votre note du controle 1er semestre :").replace(",","-"))
+ controle2 = float(input("entrez votre note du controle 2eme semestre :").replace(",","-"))
+ m = Math * 7
+ pc = pc * 7
+ s = svt * 5
+ a = anglais * 2
+ ph = philo * 2
+ total = m + pc + s + a + ph
+ national = total / 23 
+ cont = controle1 + controle2
+ contcontinue = cont / 2
+ regcont = regional + contcontinue 
+ divregcont = regcont / 2
+ moyennegeneral = national + divregcont
+ lanotetotale = moyennegeneral / 2
+ if lanotetotale >= 10:
+  print("nje7ti",round(lanotetotale ,2))
+ elif lanotetotale < 10 and lanotetotale > 7 :
+  print("rattrapage",round(lanotetotale ,2))
+ else:
+  print("manje7tich",round(lanotetotale ,2))
+
+  ## bac science svt
+
+elif choice == "2":
+    Math = float(input("Math :").replace(",", "."))
+    philo = float(input("Philosophie :").replace(",", "."))
+    pc = float(input("Physique :").replace(",", "."))
+    svt = float(input("science de la vie et de la terre :").replace(",", "."))
+    anglais = float(input("Anglais :").replace(",", "."))
+    regional = float(input("entrez vote note du regional :").replace(",", "."))
+    controle1 = float(input("entrez votre note du controle 1er semestre :").replace(",", "."))
+    controle2 = float(input("entrez votre note du controle 2eme semestre :").replace(",","-"))
+    m = Math * 7
+    pc = pc * 5
+    s = svt * 7
+    a = anglais * 2
+    ph = philo * 2
+    total = m + pc + s + a + ph
+    national = total / 23
+    cont = controle1 + controle2
+    contcontinue = cont / 2
+    regcont = regional + contcontinue
+    divregcont = regcont / 2
+    moyennegeneral = national + divregcont
+    lanotetotale = moyennegeneral / 2
+    if lanotetotale >= 10:
+        print("nje7ti", round(lanotetotale, 2))
+    elif lanotetotale < 10 and lanotetotale > 7:
+        print("rattrapage", round(lanotetotale, 2))
+    else:
+        print("manje7tich", round(lanotetotale, 2))
 else:
- print("manje7tich",lanotetotale)
+ print("invalid choice")
